1f97c8b0b1e52d595d1d03978b802352aea6bb51
dhurleystratton <dhurleystratton@gmail.com>
2025-05-27 10:22:27 -0400

    Add RealGoogleSearcher for actual Google scraping

--- src/search/__init__.py
+++ src/search/__init__.py
@@ -1,4 +1,5 @@
 from .search_query_builder import SearchQueryBuilder
 from .google_searcher import GoogleSearcher
+from .real_google_searcher import RealGoogleSearcher
 
-__all__ = ["SearchQueryBuilder", "GoogleSearcher"]
+__all__ = ["SearchQueryBuilder", "GoogleSearcher", "RealGoogleSearcher"]
